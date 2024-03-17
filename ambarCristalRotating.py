154f4d82670cc0eebf8de847102477e3e437d312
ricardo <nocive.science@gmail.com>
2024-03-17 02:02:40 -0300

    funciona pero se mueven muy rapido

--- ambarCristalRotating.py
+++ ambarCristalRotating.py
@@ -11,7 +11,9 @@ class Ambar(Scene):
         square=Square(side_length=self.conf['side_length'])
         self.play(Create(square))
         dots=self.get_dots(square)
-        self.get_anim(dots)
+        DOTS=self.get_anim(dots)
+        self.add(DOTS)
+        self.wait(10)
     def get_dots(self,square):
         positions=np.array([
             [-square.side_length/2*np.random.uniform(-.8,.8),square.side_length/2*np.random.uniform(-.8,.8),0]
@@ -31,27 +33,20 @@ class Ambar(Scene):
             dots.add(dot)
         return dots
     def get_anim(self,dots):
-        time=0
-        while time<10:
-            positions=[]
-            for particle in dots:
-                posicion=rotate_vector(0.08*RIGHT,2*np.pi*np.random.random())+particle.get_center()+0.01*RIGHT
-                if posicion[0]>self.conf['side_length']/2-self.conf['radio']:
-                    posicion[0]=-1
-                if posicion[0]<-self.conf['side_length']/2+self.conf['radio']:
-                    posicion[0]=1
-                if posicion[1]>self.conf['side_length']/2-self.conf['radio']:
-                    posicion[1]=-1
-                if posicion[1]<-self.conf['side_length']/2+self.conf['radio']:
-                    posicion[1]=1
-                positions.append(posicion)
-            myTime=.5
-            time+=myTime
-            self.play(*it.chain(*[
-                    (particle.animate.rotate(TAU*.2*np.random.random())
-                    .move_to(pos)
-                    ,)
-                    for particle, pos in zip(dots, positions)
-                ]), run_time=myTime
-            )
-        self.wait()
+        DOTS=VGroup()
+        for dot in dots:
+            dot.add_updater(self.update_particle)
+            DOTS.add(dot)
+        return DOTS
+    def update_particle(self,particle,dt):
+        posicion=rotate_vector(0.08*RIGHT,2*np.pi*np.random.random())+particle.get_center()+0.001*RIGHT
+        if posicion[0]>self.conf['side_length']/2-self.conf['radio']:
+            posicion[0]=-1
+        if posicion[0]<-self.conf['side_length']/2+self.conf['radio']:
+            posicion[0]=1
+        if posicion[1]>self.conf['side_length']/2-self.conf['radio']:
+            posicion[1]=-1
+        if posicion[1]<-self.conf['side_length']/2+self.conf['radio']:
+            posicion[1]=1
+        particle.move_to(posicion)
+        particle.rotate(TAU*.02*np.random.random())
